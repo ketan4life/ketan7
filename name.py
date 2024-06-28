a4786fddfc58c3f6543494c212af9c3f62fc8a4d
yash <yashrajrathor2005@gmail.com>
2024-06-28 11:40:08 +0530

    name

---
+++ name.py
@@ -0,0 +1,16 @@
+# name
+
+name = "Ketan"
+print("my name is" ,name)
+
+
+# This program adds two numbers
+
+num1 = 20
+num2 = 30
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
